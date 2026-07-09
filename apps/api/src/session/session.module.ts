d4e38dac043f23fc0de54c524adffae463ac3676
Kongnyu Roger <kongnyuroger711@gmail.com>
2026-07-09 14:42:15 +0100

    feat(state): add state transition validator

--- apps/api/src/session/session.module.ts
+++ apps/api/src/session/session.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { SessionService } from './session.service';
+import { StateTransitionService } from './state-transition.service';
 
 @Module({
-  providers: [SessionService],
-  exports: [SessionService],
+  providers: [SessionService, StateTransitionService],
+  exports: [SessionService, StateTransitionService],
 })
 export class SessionModule {}
